7692885960c523a844470977d91e7bde8230b43c
Eliseo F. Arévalo <eliseoarevalo29@gmail.com>
2023-09-08 19:59:16 -0600

    chore: :sparkles: add generic repository

---
+++ TodoApp.DAL/Repositories/IGenericRepository.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TodoApp.DAL.Repositories
+{
+    public interface IGenericRepository<TEntityModel> where TEntityModel : class
+    {
+        Task<bool> Insertar(TEntityModel model);
+        Task<bool> Eliminar(int id);
+        Task<IQueryable<TEntityModel>> ObtenerTodos();
+
+    }
+}
