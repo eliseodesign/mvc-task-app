992add24490cad9927c63357e3dc3e31121d2d37
Eliseo F. Arévalo <eliseoarevalo29@gmail.com>
2023-09-08 19:50:10 -0600

    :tada: initial commit and config entity framework

---
+++ TodoApp.DAL/DataContext/MvcTodoContext.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using Microsoft.EntityFrameworkCore;
+using TodoApp.EN;
+
+namespace TodoApp.DAL.DataContext;
+
+public partial class MvcTodoContext : DbContext
+{
+    public MvcTodoContext()
+    {
+    }
+
+    public MvcTodoContext(DbContextOptions<MvcTodoContext> options)
+        : base(options)
+    {
+    }
+
+    public virtual DbSet<Tarea> Tareas { get; set; }
+
+    protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+    {
+
+    }
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        modelBuilder.Entity<Tarea>(entity =>
+        {
+            entity.HasKey(e => e.Id).HasName("PK__Tarea__3214EC075FB2F164");
+
+            entity.ToTable("Tarea");
+
+            entity.Property(e => e.Completada).HasDefaultValueSql("((0))");
+            entity.Property(e => e.Descripcion)
+                .HasMaxLength(100)
+                .IsUnicode(false);
+            entity.Property(e => e.FechaRegistro)
+                .HasDefaultValueSql("(getdate())")
+                .HasColumnType("datetime");
+            entity.Property(e => e.Nombre)
+                .HasMaxLength(50)
+                .IsUnicode(false);
+        });
+
+        OnModelCreatingPartial(modelBuilder);
+    }
+
+    partial void OnModelCreatingPartial(ModelBuilder modelBuilder);
+}
---
+++ TodoApp.EN/Tarea.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace TodoApp.EN;
+
+public partial class Tarea
+{
+    public int Id { get; set; }
+
+    public string Nombre { get; set; } = null!;
+
+    public string? Descripcion { get; set; }
+
+    public DateTime? FechaRegistro { get; set; }
+
+    public bool? Completada { get; set; }
+}
